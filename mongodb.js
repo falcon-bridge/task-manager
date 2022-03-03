393f6b715dd3e2818160978278f8c160db6de771
Aayush <falcon.bridge2000@gmail.com>
2022-03-03 15:47:42 +0530

    used insertMany

--- mongodb.js
+++ mongodb.js
@@ -15,16 +15,29 @@ MongoClient.connect(
     // console.log("connected correctly");
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne(
-      {
-        name: "Tiyush",
-        age: 22,
-      },
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Tiyush",
+    //     age: 22,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert document");
+    //     }
+    //     // console.log("ok");
+    //     console.log(result);
+    //   }
+    // );
+
+    db.collection("users").insertMany(
+      [
+        { name: "Aayush", age: 22 },
+        { name: "Piyush", age: 21 },
+      ],
       (error, result) => {
         if (error) {
-          return console.log("Unable to insert document");
+          return console.log("unable to insert documents");
         }
-        // console.log("ok");
         console.log(result);
       }
     );
