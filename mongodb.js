ca6a4c0ddd949250f4520e9d77ae596642ed2ca4
Aayush <falcon.bridge2000@gmail.com>
2022-03-07 19:11:07 +0530

    used delete operation

--- mongodb.js
+++ mongodb.js
@@ -17,17 +17,26 @@ MongoClient.connect(
     // console.log("connected correctly");
     const db = client.db(databaseName);
 
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("62227887ccd1eeb5276d658d"),
+    //     },
+    //     {
+    //       $set: {
+    //         name: "Raunak",
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
     db.collection("users")
-      .updateOne(
-        {
-          _id: new ObjectID("62227887ccd1eeb5276d658d"),
-        },
-        {
-          $set: {
-            name: "Raunak",
-          },
-        }
-      )
+      .deleteMany({ age: 21 })
       .then((result) => {
         console.log(result);
       })
