63bbaa20bed8461f696fb55f21980bfb9aac9c07
Aayush <falcon.bridge2000@gmail.com>
2022-03-10 00:55:52 +0530

    created another end point for adding tasks using POST request

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,16 +3,3 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
 });
-
-// const me = new User({
-//   name: "   Aayush     ",
-//   email: "KUMAR.RKT2020@GMAIL.COM",
-// });
-
-// me.save()
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,16 +10,29 @@ app.use(express.json()); //automaticall parses the incoming JSON to a JS object
 
 app.post("/users", (req, res) => {
   const user = new User(req.body);
-  console.log(user);
+  //   console.log(user);
   user
     .save()
     .then(() => {
       //   console.log(u);
-      res.send(user);
+      res.status(201).send(user);
+    })
+    .catch((error) => {
+      //   res.status(400);
+      //   res.send(error);
+      res.status(400).send(error);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send(error);
-      //   res.send(error);
     });
 });
 
