536955e30c4ae8b66e01ad87c5aef52c90313971
Aayush <falcon.bridge2000@gmail.com>
2022-03-09 19:16:23 +0530

    added some basic data validations and sanitizations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,13 +8,34 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(email) {
+      if (!validator.isEmail(email)) {
+        throw new Error("Email is invalid");
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age can't be negative");
+      }
+    },
   },
 });
 
-const me = new User({ name: "Aayush", age: 22 });
+const me = new User({
+  name: "   Aayush     ",
+  email: "KUMAR.RKT2020@GMAIL.COM",
+});
 
 me.save()
   .then((result) => {
