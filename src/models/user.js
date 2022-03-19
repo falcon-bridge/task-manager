d0e382aa4eb03efcae9fad273939a9d3085782cf
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-19 19:00:51 +0530

    added a route for logging the users in

--- src/models/user.js
+++ src/models/user.js
@@ -9,6 +9,8 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    index: true,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -34,6 +36,24 @@ const userSchema = new mongoose.Schema({
     },
   },
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+//hash the plain text password before saving
 
 userSchema.pre("save", async function (next) {
   const user = this;
