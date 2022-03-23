178fedf76fc827a100401c4b4975a8cffeaf3dda
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-23 15:57:47 +0530

    deleted the route of getting any user data as thhe details of any users shouldn't be shown to other users and for getting his data, we already have a route

--- src/routers/user.js
+++ src/routers/user.js
@@ -109,19 +109,19 @@ router.get("/users/me", auth, async (req, res) => {
 //     });
 // });
 
-router.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
+// router.get("/users/:id", async (req, res) => {
+//   const _id = req.params.id;
 
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
+//   try {
+//     const user = await User.findById(_id);
+//     if (!user) {
+//       return res.status(404).send();
+//     }
+//     res.send(user);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
 
 router.patch("/users/:id", async (req, res) => {
   const updates = Object.keys(req.body);
