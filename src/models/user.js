68791b3d487b6146e72dd6ac260dfad3096a681b
Aayush Kumar <falcon.bridge2000@gmail.com>
2022-03-23 15:52:45 +0530

    automated the manual ptocess of hiding the private data of user using toJSON instance method

--- src/models/user.js
+++ src/models/user.js
@@ -74,7 +74,17 @@ userSchema.methods.generateAuthToken = async function () {
   return token;
 };
 
-userSchema.methods.getPublicProfile = function () {
+// userSchema.methods.getPublicProfile = function () {
+//   const user = this;
+//   const userObject = user.toObject();
+
+//   delete userObject.password;
+//   delete userObject.tokens;
+
+//   return userObject;
+// };
+
+userSchema.methods.toJSON = function () {
   const user = this;
   const userObject = user.toObject();
 
